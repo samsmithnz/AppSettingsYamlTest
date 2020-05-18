dd561ba810d895464615f0d88cde2508a1cff892
Sam Smith <samsmithnz@gmail.com>
2020-05-17 21:55:06 -0400

    Sending to GitHub

---
+++ AppSettingsYamlTest/AppSettingsYamlTest.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AppSettingsYamlTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public IConfigurationRoot Configuration;
+
+        [TestInitialize]
+        public void TestStartUp()
+        {
+            IConfigurationBuilder config = new ConfigurationBuilder()
+               .SetBasePath(AppContext.BaseDirectory)
+               .AddJsonFile("appsettings.json");
+            Configuration = config.Build();
+        }
+
+
+        [TestMethod]
+        public void ShouldFailInGitHubActions()
+        {
+
+            Assert.AreEqual("1977 was the best year", Configuration["AppSettings:MySecret"]);
+        }
+
+        [TestMethod]
+        public void ShouldFailLocally()
+        {
+
+            Assert.AreEqual("2020 is the worst year", Configuration["AppSettings:MySecret"]);
+        }
+    }
+}
